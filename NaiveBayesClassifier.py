099396132e38ffb5f15b570df66a253d30ac5275
averybogusz <56933616+averybogusz@users.noreply.github.com>
2019-10-25 22:56:58 -0400

    Changed to allow more general distributions
    Allows more general distributions. Since we're dealing with arbitrary, user-defined pdfs it is now computed in terms of probability, rather than log of the probability. Additionally, removed the main function "check" at the bottom, that would be better off in a different file.

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -2,10 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal as mvn
 
-def accuracy(y,y_hat):
-    return np.mean( y== y_hat)
+'''
+The generalized NaiveBayesClassifier assumes a user-supplied pdf, written as a function. This function should take a 
+in data, a vector of means, and a variance-covariance matrix. I suppose it could also simply take data and the means in certain cases. 
+Anything more complex, ie, if you want to define the shape of the distribution by some function of sigma and the means, rather than directly, 
+will require changes within your pdf as the code isn't able to handle that. 
+'''
+
+
 
 class NaiveBayesClassifier():
+
     def fit(self, X, y, naive=0):
         self.likelihoods =dict()
         self.priors = dict()
@@ -13,46 +20,30 @@ class NaiveBayesClassifier():
 
         for k in self.K:
             X_k  = X[y==k]
-            self.likelihoods[k] = {"mu" : X_k.mean(axis = 0), "SIGMA": (1 - naive) * np.cov(X_k.T) + naive * np.diag(np.diag(np.cov(X_k.T)))} #note the Naive indicator will simply tell python whether to grab the covariance matrix, or its diagonal
+            self.likelihoods[k] = {"mu" : X_k.mean(axis = 0), "SIGMA": (1 - naive) * np.cov(X_k.T) + naive * np.diag(np.diag(np.cov(X_k.T)))} #features independent of one another
             self.priors[k] = len(X_k)/len(X)
         pass
 
-    def predict(self, X): #not the same X as before, though it still needs D columns.
+    def predict(self, X, user_pdf = mvn.pdf, include_sigma = 1): #not the same X as before, though it still needs D columns.
+
+        # input_pdf allows the user to specify an arbitrary pdf that takes either some data and the mean of the distribution
+        # or, alternatively, the data, the mean, and the variance-covariance matrix (variance in the univariate case).
+        # I'm not really sure about generalizing further since you can define so many distributions by so many different parameters.
+        def input_pdf(*args, user_pdf, include_sigma):
+            X_data=args[0]
+            mu_mat=args[1]
+            sigma_mat=args[2]
+            if include_sigma == 1:
+                return user_pdf(*args)
+            else:
+                return user_pdf(args[0], args[1])
 
         P = np.zeros((len(X), len(self.K)))
 
         for k,l in self.likelihoods.items(): #k is the label, l is the parameters, going through unpacking dictionary
-            P[:,k] = mvn.logpdf(X, l["mu"], l["SIGMA"]) + np.log(self.priors[k])# fixing k, we'll store these in the right columns. remember mu and sigma are vectors
+            P[:,k] = input_pdf(X, l["mu"], l["SIGMA"], user_pdf = user_pdf, include_sigma = include_sigma) * self.priors[k] # fixing k, we'll store these in the right columns. remember mu and sigma are vectors
 
         return P.argmax(axis=1)
 
         pass
 
-
-ex_dict = {0: {"mu":70, "SIGMA":40}, 1: {"mu":63, "SIGMA":7}} # this is basically how the dictionary works, each number is a k im pretty sure
-for k, l in ex_dict.items():
-    print(k)
-    print(l)
-
-def main():
-    D = 2
-    K = 3
-    N = int(K * 1e3)
-
-    X0 = np.random.randn((N//K), D) + np.array([2,2])
-    X1 = np.random.randn((N//K),D) + np.array([0,-2])
-    X2 = np.random.randn((N//K),D) + np.array([-2,2])
-    X = np.vstack((X0, X1, X2))
-    y = np.array([0]*(N//K) + [1] *(N//K) + [2] * (N//K))
-
-    plt.figure()
-    plt.scatter(X[:,0], X[:,1], c = y, alpha = .5)
-
-    nb = NaiveBayesClassifier()
-    nb.fit(X,y, naive=0)
-    y_hat = nb.predict(X)
-    print(f"Training accuracy: {accuracy(y, y_hat):0.4f}")
-    pass
-
-if __name__ == "__main__":
-    main()
